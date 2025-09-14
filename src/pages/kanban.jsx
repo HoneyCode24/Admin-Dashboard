87df1b6d6719bafcd04868070b515cbdb2b96bf2
HoneyCode24 <honeycodes24@gmail.com>
2025-09-14 09:14:04 +0530

    Update kanban.jsx

--- src/pages/kanban.jsx
+++ src/pages/kanban.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-Kanban'
+import { KanbanComponent , ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-react-kanban";
 import { kanbanData, kanbanGrid } from '../data/dummy'
 import { Header } from '../components'
 const kanban = () => {
